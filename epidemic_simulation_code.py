aa9ac8f2d13255cfc8a230e9696b59d069dac54b
Stephen Merry <57818509+stephen-merry@users.noreply.github.com>
2020-01-04 21:23:48 +1300

    Finished the first iteration

--- epidemic_simulation_code.py
+++ epidemic_simulation_code.py
@@ -5,16 +5,31 @@ Created on Sat Jan  4 15:42:19 2020
 
 @author: stephen
 """
+import population_creation as pop_crtn
 import epidemic_simulation as epi_sim
 
+EXPOSURE_RATE = 3/365
+INFECTIOUS_RATE = 5/365
+REMOVAL_RATE = 3/365
+
+list_of_individuals = [str(i) for i in range(0, 40)]
+initial_exposed = ['0', '3', '6', '8']
+weighted_adjacency_matrix = pop_crtn.create_dummy_weighted_adjacency_matrix(40) / 10
+
+# Initialise Population
+weighted_adjacency_list = pop_crtn.create_weighted_adjacency_list(
+        list_of_individuals, weighted_adjacency_matrix)
+
 # Initialise Epidemic
+exposure_rates, infectious_rates, removal_rates = epi_sim.create_identical_rate_arrays(len(list_of_individuals), EXPOSURE_RATE,
+                                       INFECTIOUS_RATE, REMOVAL_RATE)
 epidemic_list = epi_sim.create_epidemic_list(list_of_individuals,
                                              initial_exposed, exposure_rates,
                                              infectious_rates, removal_rates)
 
 # Run Gillespie Algorithm
-epidemic_events = epi_sim.execute_gillespie_algorithm(epidemic_list)
+epidemic_events = epi_sim.execute_gillespie_algorithm(epidemic_list,
+                                                      weighted_adjacency_list)
 
 # Visualise Epidemic spread
-
-
+epi_sim.print_out_epidemic_events(epidemic_events, 0.1)
--- population_creation.py
+++ population_creation.py
@@ -8,15 +8,34 @@ Created on Fri Jan  3 11:25:23 2020
 
 import numpy as np
 
+def create_dummy_weighted_adjacency_matrix(num_individuals):
+    """Creates a symmetric dummy weighted adjacency matrix that we can use to 
+    test our simulations on."""
+    weighted_adjacency_matrix = (np.random.rand(num_individuals,
+                                               num_individuals) * 2)
+    weighted_adjacency_matrix[weighted_adjacency_matrix > 0.4] = 0
+    for row in range(0, num_individuals):
+        for col in range(row, num_individuals):
+            if row == col:
+                weighted_adjacency_matrix[row][col] = 0
+                weighted_adjacency_matrix[col][row] = 0
+            else:
+                weighted_adjacency_matrix[col][row] = weighted_adjacency_matrix[row][col]    
+    return weighted_adjacency_matrix
+    
 def create_weighted_adjacency_list(individual_list, weighted_adjacency_matrix):
+    """From a weighted adjacency matrix, we get a list of tuples representing 
+    edges and edge weights"""
     nonzero_indices = np.nonzero(weighted_adjacency_matrix)
     weighted_adjacency_list = [] 
 
     for i in range(0, len(nonzero_indices[0])):
         source_node = nonzero_indices[0][i]
         sink_node = nonzero_indices[1][i]
-        weighted_adjacency_list.append([individual_list[source_node],
-                        individual_list[sink_node],
+        weighted_adjacency_list.append([(individual_list[source_node],
+                        individual_list[sink_node]),
                         weighted_adjacency_matrix[source_node, sink_node]])
     
     return weighted_adjacency_list
+
+
